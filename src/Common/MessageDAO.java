038773e6f6f7488477f1f6ea05a8f18db5ff13ff
acaec <876036615@qq.com>
2016-01-03 16:11:40 +0800

    课件和通知的后端数据库操作
    通知数据库
    CREATE table `SEWeb`.`Message` (
    `ID`  int not null,
    `CreateAt` DATETIME NULL,
    `Title` VARCHAR(10897) NOT NULL,
    `Content` VARCHAR(10897) NULL,
    `Important` BIT(1) NULL,
    `IsRead` BIT(1) NULL,
    PRIMARY KEY (`ID`));
    课件数据库
    CREATE TABLE `seweb`.`resource` (
    `ID` INT NOT NULL,
    `CreateAt` DATETIME NULL,
    `FileName` VARCHAR(128) NOT NULL,
    `Description` VARCHAR(10897) NULL,
    `FileUrl` VARCHAR(128) NULL,
    PRIMARY KEY
    (`ID`));
    完善了下database的数据库读取操作；通知和消息的各自属性分配在Message和Resource类中；MessageMapper和ResourceMapper为对接口RowMapper的各自实现，用于将数据库中读取的信息封装为单独的类；将对通知和课件的各项数据库操作作为静态方法写在MessageDAO和ResourceDAO中，可以以类名直接调用。

---
+++ src/Common/MessageDAO.java
@@ -0,0 +1,43 @@
+package Common;
+
+import model.Database;
+import model.Message;
+import model.MessageMapper;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/1/3.
+ */
+public class MessageDAO {
+    public static void sendMessage(Database database, Message message){
+        String sql="insert into Message (ID, CreateAt, Title, Content, Important, IsRead) values ("+message.getId()+",'"+ message.getTime() +"','"+message.getSubject()+"','"+message.getContent()+"',"+message.isImportant()+","+message.isRead()+")";
+        database.insert(sql);
+    }
+    public static void updateMessage(Database database,int oldId,Message newMessage){
+        String sql="update Message set ID="+newMessage.getId()+",CreateAt='"+newMessage.getTime()+"',Title='"+newMessage.getSubject()+"',Content='"+newMessage.getContent()+"',Important="+newMessage.isImportant()+",IsRead="+newMessage.isRead()+" where ID="+ oldId;
+        database.excute(sql);
+    }
+    public static void deleteMessage(Database database,int id){
+        String sql="delete from Message where ID="+id;
+        database.excute(sql);
+    }
+    public static Message getMessage(Database database,int id){
+        String sql="select * from Message where ID= ?";
+        Message message = (Message)database.get(sql,new Object[]{id},new MessageMapper());
+        return message;
+    }
+    public static List<Message> getMessageList(Database database){
+        String sql = "select * from Message";
+        List <Message> MessageList = database.getList(sql,new MessageMapper());
+        return MessageList;
+    }
+    //test
+    public static void main(String[] args){
+        Database database = (Database)new FileSystemXmlApplicationContext("/web/WEB-INF/SEWeb-servlet.xml").getBean("database");
+        //Message message=new Message("ttt","ttt");
+        List <Message> list=MessageDAO.getMessageList(database);
+    }
+}
+
--- src/model/Database.java
+++ src/model/Database.java
@@ -2,11 +2,13 @@ package model;
 
 import org.springframework.core.ExceptionDepthComparator;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 
 import javax.sql.DataSource;
+import java.util.List;
 
 
 public class Database {
@@ -26,6 +28,14 @@ public class Database {
     }
 
     public void excute(String sql) {jdbcTemplate.execute(sql);}
+
+    public Object get (String sql,Object[] args,RowMapper rowMapper){
+        return jdbcTemplate.queryForObject(sql,args,rowMapper);
+    }
+
+    public List getList (String sql, RowMapper rowMapper){
+        return jdbcTemplate.query(sql,rowMapper);
+    }
 
     public boolean exists(String sql) {
 
